89940df78d1af9d18c4b170c478d71e8bd051167
Arda İlhan <ardailhan7@gmail.com>
2024-02-05 15:18:21 +0300

    Error fixed in program.cs

--- Order.API/Program.cs
+++ Order.API/Program.cs
@@ -1,25 +1,46 @@
+using MassTransit;
+using Microsoft.EntityFrameworkCore;
+using Order.API.Models.Contexts;
+using Order.API.ViewModels;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
 
-builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+builder.Services.AddMassTransit(configurator =>
+{
+    configurator.UsingRabbitMq((context, _configure) =>
+    {
+        _configure.Host(builder.Configuration["RabbitMQ"]);
+    });
+});
+
+builder.Services.AddDbContext<OrderAPIDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("MSSQLServer")));
+
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+app.UseSwagger();
+app.UseSwaggerUI();
+
+app.MapPost("/create-order", async (CreateOrderVM model, OrderAPIDbContext context) =>
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
-
-app.UseHttpsRedirection();
-
-app.UseAuthorization();
-
-app.MapControllers();
+    Order.API.Models.Order order = new()
+    {
+        BuyerId = Guid.TryParse(model.BuyerId, out Guid _buyerId) ? _buyerId : Guid.NewGuid(),
+        OrderItems = model.OrderItems.Select(oi => new Order.API.Models.OrderItem()
+        {
+            Count = oi.Count,
+            Price = oi.Price,
+            ProductId = Guid.Parse(oi.ProductId)
+        }).ToList(),
+        OrderStatus = Order.API.Enums.OrderStatus.Suspend,
+        CreatedDate = DateTime.UtcNow,
+        TotalPrice = model.OrderItems.Sum(oi => oi.Price * oi.Count),
+    };
+    await context.AddAsync(order);
+    await context.SaveChangesAsync();
+});
 
 app.Run();
