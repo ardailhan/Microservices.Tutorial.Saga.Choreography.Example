a8c03f2d3c430db9e41472d82bf555a03f308e26
Arda İlhan <ardailhan7@gmail.com>
2024-02-09 11:35:39 +0300

    All consumers done

---
+++ Stock.API/Consumers/PaymentFailedEventConsumer.cs
@@ -0,0 +1,24 @@
+﻿using MassTransit;
+using MongoDB.Driver;
+using Shared.Events;
+using Stock.API.Services;
+
+namespace Stock.API.Consumers
+{
+    public class PaymentFailedEventConsumer(MongoDBService _mongoDBService) : IConsumer<PaymentFailedEvent>
+    {
+        public async Task Consume(ConsumeContext<PaymentFailedEvent> context)
+        {
+            var stocks = _mongoDBService.GetCollection<Models.Stock>();
+            foreach (var orderItem in context.Message.OrderItems)
+            {
+                var stock = await (await stocks.FindAsync(s => s.ProductId == orderItem.ProductId)).FirstOrDefaultAsync();
+                if (stock != null)
+                {
+                    stock.Count += orderItem.Count;
+                    await stocks.FindOneAndReplaceAsync(s => s.ProductId == orderItem.ProductId, stock);
+                }
+            }
+        }
+    }
+}
